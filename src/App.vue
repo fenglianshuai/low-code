4e9b1e85a62219ab0d8b5c5a1fc790778ffbfa3b
unknown <fengls@yiche.com>
2023-07-27 11:34:16 +0800

    feat: new barnch

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,7 @@
-<template>
-  <Editor v-model="state" :formData="formData"></Editor>
-</template>
+<template></template>
 
 <script setup>
-import { provide, reactive, ref } from 'vue'
-import data from './data.json';
-import Editor from './views/editor';
-import { registerConfig as config } from './components/editor-config';
 
-provide('config', config);
-const state = ref(data);
-const formData = reactive({
-  default: '123123',
-  start: 0,
-  end: 100
-})
 </script>
 
 <style scoped></style>
